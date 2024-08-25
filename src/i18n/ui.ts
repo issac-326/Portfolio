4558f46d213760c7273ab7737096b2d883969a22
issac-326 <issacjamaelc00@gmail.com>
2024-08-24 22:41:58 -0600

    first commit

---
+++ src/i18n/ui.ts
@@ -0,0 +1,30 @@
+export const languages = {
+    es: 'Español',
+    en: 'English',
+  };
+  
+  export const defaultLang = 'es';
+  
+  export const ui = {
+    en: {
+      'nav.aboutMe': 'About me',
+      'nav.img': 'https://flagcdn.com/us.svg',
+      'nav.url': '/es',
+      'nav.skills': 'Skills',
+      'nav.proyects': 'Proyects',
+      'text.aboutMe': `I've always been fascinated by computers. Understanding how programs work and why they behave as they do has kept my curiosity alive, and this passion led me to pursue a degree in computer engineering.`,
+      'text.aboutMe2': 'I consider myself an enthusiastic learner in the field of computer science, always eager to explore new areas. I have the ability to create and collaborate within a team, and I continuously strive to improve myself while keeping an open mind to the latest professional trends.',
+      'text.aboutMe3': 'Outside of studying and programming, I spend my free time reading books, learning new skills, and playing video games.',
+
+    },
+    es: {
+      'nav.aboutMe': 'Sobre mi',
+      'nav.url': '/en',
+      'nav.img': `https://flagcdn.com/es.svg`,
+      'nav.skills': 'Habilidades',
+      'nav.proyects': 'Proyectos',
+      'text.aboutMe':`Desde siempre, la fascinación por las computadoras ha sido una constante en mi vida. El funcionamiento de los programas y el porqué de sus acciones han alimentado mi curiosidad, y es esta pasión la que me llevó a estudiar ingeniería en sistemas.`,
+      'text.aboutMe2': 'Me considero un entusiasta de la informática, siempre dispuesto a aprender y explorar nuevas áreas del campo. Tengo la habilidad de crear y colaborar en equipo, y busco superarme constantemente mientras mantengo una mente abierta a las últimas tendencias profesionales.',
+      'text.aboutMe3': 'Fuera del estudio y la programación, disfruto de mi tiempo libre leyendo libros, aprendiendo nuevas habilidades y jugando videojuegos.',
+    },
+  } as const;
