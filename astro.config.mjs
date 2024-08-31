14071e2ad62829d996ca465ed71feaea937ce846
issac-326 <issacjamaelc00@gmail.com>
2024-08-30 21:03:13 -0600

    servidor2

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,9 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
   integrations: [tailwind({
     applyBaseStyles: false,
   }), react()],
-  output: 'server',
-  adapter: vercel(),
 })
