8c076e39933ded8757347eec1f2a693009b22b9f
issac-326 <issacjamaelc00@gmail.com>
2024-12-01 19:21:55 -0600

    no twitter

--- src/pages/components/Footer.tsx
+++ src/pages/components/Footer.tsx
@@ -41,7 +41,7 @@ export default function Footer() {
                         </svg>
                     }
                 />
-                <SocialButton 
+                {/* <SocialButton 
                     href="https://twitter.com/your-twitter-handle" 
                     ariaLabel="Twitter profile"
                     icon={
@@ -50,7 +50,7 @@ export default function Footer() {
                             <path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z"/>
                         </svg>
                     }
-                />
+                /> */}
             </div>
         </section>
     )
